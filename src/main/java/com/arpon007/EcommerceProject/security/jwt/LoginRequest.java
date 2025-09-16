61b2fd300bd7ecddcf4f8d547926f28ce09d92a5
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-09-17 00:13:15 +0600

    security added

---
+++ src/main/java/com/arpon007/EcommerceProject/security/jwt/LoginRequest.java
@@ -0,0 +1,23 @@
+package com.arpon007.EcommerceProject.security.jwt;
+
+public class LoginRequest {
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
