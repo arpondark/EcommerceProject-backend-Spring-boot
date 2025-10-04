7d4109749b4776eb8f604060ae93a5fac879f7c0
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-10-04 23:27:21 +0600

    fetch user cart added

--- src/main/java/com/arpon007/EcommerceProject/controller/CartController.java
+++ src/main/java/com/arpon007/EcommerceProject/controller/CartController.java
@@ -1,7 +1,10 @@
 package com.arpon007.EcommerceProject.controller;
 
+import com.arpon007.EcommerceProject.Utils.AuthUtil;
+import com.arpon007.EcommerceProject.model.Cart;
 import com.arpon007.EcommerceProject.payload.CartDTO;
 import com.arpon007.EcommerceProject.payload.CartItemsDTO;
+import com.arpon007.EcommerceProject.repositories.CartRepository;
 import com.arpon007.EcommerceProject.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class CartController {
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    AuthUtil authUtil;
 
 
     @Autowired
@@ -28,6 +36,16 @@ public class CartController {
     public ResponseEntity<List<CartDTO>> getCarts() {
         List<CartDTO> cartDTOs = cartService.getAllCarts();
         return new ResponseEntity<List<CartDTO>>(cartDTOs, HttpStatus.FOUND);
+    }
+
+    @GetMapping("carts/users/cart")
+    public ResponseEntity<CartDTO> getCartById() {
+        String emailId = authUtil.loggedInEmail();
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        Long cartId = cart.getCartId();
+       CartDTO cartDTO = cartService.getCart(emailId,cartId);
+       return new ResponseEntity<CartDTO>(cartDTO,HttpStatus.OK);
+
     }
 
 
